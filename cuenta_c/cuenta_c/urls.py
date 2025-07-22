67748968834918934ccfb905575b8d8401e3d79c
bravo-666 <bravo.emmanuel666@gmail.com>
2025-07-22 13:16:53 -0600

    Nuevo usuario

--- cuenta_c/cuenta_c/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('usuarios.urls')),  # redirige todo a la app usuarios
-]
